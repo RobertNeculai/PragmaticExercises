1bdaee9a3448ddd222e8a658d5acf5060bc3fa99
Neculai Robert <robert.neculai13@gmail.com>
2019-12-01 19:59:06 +0200

    ex2

--- src/main/java/inputprocessingoutput/Counting_The_Number_Of_Characters.java
+++ src/main/java/inputprocessingoutput/Counting_The_Number_Of_Characters.java
@@ -1,4 +1,16 @@
 package inputprocessingoutput;
 
+import java.util.Scanner;
+
 public class Counting_The_Number_Of_Characters {
+    public static void main( String[] args )
+    {
+        System.out.println("What is the imput string ? ");
+        Scanner scanner=new Scanner(System.in);
+        String scan=scanner.next();
+        if(scan.length()!=0)
+        System.out.println("Input string "+scan+" has "+scan.length()+" characters");
+        else
+            System.out.println("Enter a string");
+    }
 }
